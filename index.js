45864bb625de66a11f88a0dd4ac1098090af3105
Anjil0 <angilneupane222@gmail.com>
2023-12-22 11:31:44 +0545

    first commit

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ app.set("view engine", "ejs");
 function fetchdata() {
   const data = fs.readFileSync("./Components/book.json");
   const dataJSON = JSON.parse(data);
+  return dataJSON;
 }
 
 app.get("/", (req, res) => {
-  fetchdata();
-  res.render("pages/index.ejs");
+  const books = fetchdata();
+  res.render("pages/index", { books: books });
 });
 
 // uploading to the website with port 1070
