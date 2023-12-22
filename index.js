323ab062c979c6dec754b0f767d9da358dd298ce
Anjil0 <angilneupane222@gmail.com>
2023-12-22 11:21:27 +0545

    day 2

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require("express"); //import expresss function
 const app = express(); // initialize express
 const route = express.Router();
+// making json file to read so requiring fs library
+const fs = require("fs");
 
 // setting viewengine to view ejs file
 app.set("view engine", "ejs");
 
-/////////////////////////////
-// app.get("/", (req, res) => {
-//   res.send("hello world");
-// });
+function fetchdata() {
+  const data = fs.readFileSync("./Components/book.json");
+  const dataJSON = JSON.parse(data);
+}
 
 app.get("/", (req, res) => {
+  fetchdata();
   res.render("pages/index.ejs");
 });
-
-
 
 // uploading to the website with port 1070
 app.listen(1070, () => {
