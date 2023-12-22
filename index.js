03896c9599e057ac66de9d1193762cb7e46f4e37
Anjil0 <angilneupane222@gmail.com>
2023-12-22 10:48:21 +0545

    ok

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express"); //import expresss function
+const app = express(); // initialize express
+const route = express.Router();
+
+// setting viewengine to view ejs file
+app.set("view engine", "ejs");
+
+/////////////////////////////
+// app.get("/", (req, res) => {
+//   res.send("hello world");
+// });
+
+app.get("/home", (req, res) => {
+  res.render("pages/index.ejs");
+});
+
+
+
+// uploading to the website with port 1070
+app.listen(1070, () => {
+  console.log("##-- Connected to the port 1070 --##");
+});
